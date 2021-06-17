0b9d43d787d6da3dd645affd1d7315244501b885
glenadamson1984 <glenadamson1984@gmail.com>
2021-06-17 09:52:34 +0100

    test for checkbox

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import { useState } from "react";
 
 function App() {
   const [buttonColor, setButtonColor] = useState("red");
+  const [disabled, setDisabled] = useState(false);
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
 
   const onClickHandler = () => {
     setButtonColor(newButtonColor);
   };
 
+  const onCheckHandler = () => {
+    setDisabled(!disabled);
+  };
+
   return (
     <div>
-      <button onClick={onClickHandler} style={{ backgroundColor: buttonColor }}>
+      <button
+        disabled={disabled}
+        onClick={onClickHandler}
+        style={{ backgroundColor: buttonColor }}
+      >
         Change to {newButtonColor}
       </button>
+      <input type="checkbox" checked={disabled} onChange={onCheckHandler} />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -14,3 +14,30 @@ test("button turns blue when clicked", () => {
   expect(button).toHaveStyle({ backgroundColor: "blue" });
   expect(button.textContent).toBe("Change to red");
 });
+
+test("button should be enabled and the checkbox starts unchecked when initial render", () => {
+  render(<App />);
+
+  const button = screen.getByRole("button", { name: /change to blue/i });
+  expect(button).toBeEnabled();
+
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+});
+
+test("check button is disabled when the checkbox is checked and enabled when unchecked", () => {
+  render(<App />);
+
+  const button = screen.getByRole("button", { name: /change to blue/i });
+  const checkbox = screen.getByRole("checkbox");
+
+  fireEvent.click(checkbox);
+
+  expect(checkbox).toBeChecked();
+  expect(button).toBeDisabled();
+
+  fireEvent.click(checkbox);
+
+  expect(checkbox).not.toBeChecked();
+  expect(button).toBeEnabled();
+});
